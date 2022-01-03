48425965e29a180e69ac2e6fe916f31e61f52f5d
DarthSigil <servantofchristvigil@mail.com>
2022-01-03 18:52:56 -0500

    sentence_length & other similar vars don't need 2 b inside while loop -> taking my time to refine & understand user so post b4 refactoring => farthest ever gotten in truly grasping a Markov Chain

--- markov.py
+++ markov.py
@@ -6,11 +6,11 @@ def Markov(text_file): # txt file 2 parse
 
     data = [i for i in data.split(' ') if i != ''] # list of all words
     data = [i.lower() for i in data if i.isalpha()] # remove punctuation
-    markov = {i:[] for i in data} # need 2 print immediately
+    markov = {i:[] for i in data} # dict words as k & empty [] as v
 
     pos = 0
    # add a word to the word-key's list if it immediately follows that word
-    while pos < len(data) - 1:
+    while pos < len(data) - 1: # -1 bcuz need 2 add next word to word/key list ([])
         markov[data[pos]].append(data[pos+1])
         pos += 1
         # create another dict for the seed to match up with
@@ -20,10 +20,11 @@ def Markov(text_file): # txt file 2 parse
         # random start point
         seed = random.randint(0, len(new) - 1)
         # use as first word as well as starting point
-        sentence_data = [new[seed]]
+        sentence_data = [new[seed]] # 2 b returned as answer
         current_word = new[seed]
 
     while len(sentence_data) < length_sentence:
+        print("random answer len: ", length_sentence)
         # randomly pick word from the last word's list
         next_index = random.randint(0, len(markov[current_word]) - 1)
         next_word = markov[current_word][next_index]
