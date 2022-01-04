0917d28dd2b9f68ed707dd98f85769713cdbe390
DarthSigil <servantofchristvigil@mail.com>
2022-01-03 19:11:30 -0500

    repositioned sentence_length, sentence_data, and current_word vars outside of while-loop, program works the same

--- markov.py
+++ markov.py
@@ -9,22 +9,21 @@ def Markov(text_file): # txt file 2 parse
     markov = {i:[] for i in data} # dict words as k & empty [] as v
 
     pos = 0
-   # add a word to the word-key's list if it immediately follows that word
-    while pos < len(data) - 1: # -1 bcuz need 2 add next word to word/key list ([])
+    # random length
+    length_sentence = random.randint(15, 20)
+    # create another dict for the seed to match up with
+    new = {k:v for k,v in zip(range(len(markov)), [i for i in markov])}
+    # random start point
+    seed = random.randint(0, len(new) - 1)
+    # use as first word as well as starting point
+    sentence_data = [new[seed]] # 2 b returned as answer
+    current_word = new[seed]
+    # add a word to the word-key's list if it immediately follows that word
+    while pos < len(data) - 1: # -1 bcuz need 2 add nxt word to word/key list ([])
         markov[data[pos]].append(data[pos+1])
         pos += 1
-        # create another dict for the seed to match up with
-        new = {k:v for k,v in zip(range(len(markov)), [i for i in markov])}
-        # random length
-        length_sentence = random.randint(15, 20)
-        # random start point
-        seed = random.randint(0, len(new) - 1)
-        # use as first word as well as starting point
-        sentence_data = [new[seed]] # 2 b returned as answer
-        current_word = new[seed]
 
     while len(sentence_data) < length_sentence:
-        print("random answer len: ", length_sentence)
         # randomly pick word from the last word's list
         next_index = random.randint(0, len(markov[current_word]) - 1)
         next_word = markov[current_word][next_index]
