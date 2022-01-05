98d49a927ca197331186c47f84dde659d0a2ca55
DarthSigil <servantofchristvigil@mail.com>
2022-01-05 02:20:57 -0500

    condensed by putting comments to the side of each line -> still has a bug -> need 2 find where and when list (arr) empties

--- markov.py
+++ markov.py
@@ -9,23 +9,17 @@ def Markov(text_file): # txt file 2 parse
     markov = {i:[] for i in data} # dict words as k & empty [] as v
 
     pos = 0
-    # random length
-    length_sentence = random.randint(15, 20)
-    # create another dict for the seed to match up with
-    new = {k:v for k,v in zip(range(len(markov)), [i for i in markov])}
-    # random start point
-    seed = random.randint(0, len(new) - 1)
-    # use as first word as well as starting point
-    sentence_data = [new[seed]] # 2 b returned as answer
+    length_sentence = random.randint(15, 20) # random length
+    new = {k:v for k,v in zip(range(len(markov)), [i for i in markov])} # create another dict for the seed to match up with
+    seed = random.randint(0, len(new) - 1) # random start point
+    sentence_data = [new[seed]] # 1st word & strtng pnt (will be answer)
     current_word = new[seed]
-    # add a word to the word-key's list if it immediately follows that word
-    while pos < len(data) - 1: # -1 bcuz need 2 add nxt word to word/key list ([])
+    while pos < len(data) - 1: # add a word to the word-key's list if it immediately follows that word
         markov[data[pos]].append(data[pos+1])
         pos += 1
 
     while len(sentence_data) < length_sentence:
-        # randomly pick word from the last word's list
-        next_index = random.randint(0, len(markov[current_word]) - 1)
+        next_index = random.randint(0, len(markov[current_word]) - 1)  # randomly pick word from the last word's list
         next_word = markov[current_word][next_index]
         sentence_data.append(next_word)
         current_word = next_word
